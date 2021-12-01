9757f7501f0b34243c0125836377922e8e43bd08
fengyuanliang3 <--show-origin>
2021-12-01 12:29:05 +0800

    feat: 默认matcher

--- packages/statics-webpack/app/middleware/webpack.js
+++ packages/statics-webpack/app/middleware/webpack.js
@@ -3,7 +3,24 @@
 const koaWebpack = require('koa-webpack');
 
 module.exports = (options, app) => {
-  const { matcher = () => {} } = app.config.statics || '';
+  const { matcher } = app.config.statics || {};
+
+  const staticsMatcher =
+    matcher && typeof matcher === 'function'
+      ? matcher
+      : (ctx) => {
+          let key = Object.keys(app.statics || {}).find((p) => {
+            return ctx.request.url.indexOf(`/${p}/`) === 0;
+          });
+
+          if (!key) {
+            key = Object.keys(app.webpackConfigs || {}).find((p) => {
+              return ctx.request.url.indexOf(`/${p}/`) === 0;
+            });
+          }
+
+          return key || app.config.statics.default;
+        };
 
   const middlewareMap = {};
 
@@ -12,7 +29,7 @@ module.exports = (options, app) => {
   });
 
   return async function (ctx, next) {
-    const matched = middlewareMap[matcher(ctx)];
+    const matched = middlewareMap[staticsMatcher(ctx)];
 
     return (matched && (await matched)(ctx, next)) || next();
   };
