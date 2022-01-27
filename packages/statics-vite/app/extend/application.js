34be27c7b68a151516a4a8215478f21209685cbf
fengyuanliang3 <--show-origin>
2022-01-27 14:50:50 +0800

    feat: 自动查找hmr可用端口

--- packages/statics-vite/app/extend/application.js
+++ packages/statics-vite/app/extend/application.js
@@ -3,6 +3,8 @@
 const path = require('path');
 const { createServer, loadConfigFromFile, mergeConfig } = require('vite');
 const fs = require('fs');
+const net = require('net');
+const os = require('os');
 
 const serviceToken = Symbol('serviceToken');
 
@@ -38,6 +40,62 @@ const getCallerFile = function () {
   Error.prepareStackTrace = pst;
 
   return path.dirname(filename);
+};
+
+const createPortDiscover = () => {
+  const checkAvailablePort = (port) => {
+    return new Promise((resolve, reject) => {
+      const server = net.createServer();
+
+      server.unref();
+
+      server.on('error', reject);
+
+      server.listen({ port, hosts }, () => {
+        const { port } = server.address();
+
+        server.close(() => {
+          resolve(port);
+        });
+      });
+    });
+  };
+
+  const getLocalHosts = () => {
+    const interfaces = os.networkInterfaces();
+
+    const results = new Set([undefined, '0.0.0.0']);
+
+    for (const inter of Object.values(interfaces)) {
+      for (const config of inter) {
+        results.add(config.address);
+      }
+    }
+
+    return results;
+  };
+
+  const getPort = async (from, callback) => {
+    const result = await checkAvailablePort(from)
+      .then((e) => e)
+      .catch((e) => e);
+
+    if (result instanceof Error) {
+      await getPort(from + 1, callback);
+    } else {
+      callback(from);
+    }
+  };
+
+  const hosts = getLocalHosts();
+
+  return async (start) => {
+    return new Promise((resolve) => {
+      getPort(start, (result) => {
+        resolve(result);
+      });
+    });
+  };
 };
 
 module.exports = {
@@ -102,6 +160,10 @@ module.exports = {
             return key || this.config.statics.default;
           };
 
+    const getWsPort = createPortDiscover();
+
+    let wsPort = 24678;
+
     await Promise.all(
       Object.keys(this.viteConfigs).map(async (key, index) => {
         if (!this._service[key]) {
@@ -117,7 +179,7 @@ module.exports = {
             return;
           }
 
-          const wsPort = 24679 + index;
+          wsPort = await getWsPort(wsPort);
 
           this._service[key] = await createServer(
             mergeConfig(viteConfig, {
