8e1ae9fb1370bd52e3d4a24d8082148f5a2486a3
fengyuanliang3 <--show-origin>
2022-04-12 11:48:49 +0800

    chore: render exclude style import

--- packages/render/src/boot.js
+++ packages/render/src/boot.js
@@ -6,24 +6,10 @@ import * as JRender from '@jrender-legacy/core'
 
 const config = external ? JSON.parse(external) : []
 
-const styles = []
-
-const existingImport = System.constructor.prototype.import
-
 window.Vue = Vue
 window.VueCompositionAPI = VueCompositionAPI
 window.JRender = JRender
 
-System.constructor.prototype.import = async function (args) {
-  const result = await Promise.resolve(existingImport.call(this, args))
-
-  if (result.default && result.default.type === 'text/css') {
-    styles.push(result.default)
-  }
-}
-
 export const boot = async () => {
-  await Promise.all(config.map((item) => System.import(item))).then(() => {
-    document.adoptedStyleSheets = [...document.adoptedStyleSheets, ...styles]
-  })
+  await Promise.all(config.map((item) => System.import(item)))
 }
